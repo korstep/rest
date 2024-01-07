6259b7e95a1ae7ef22952c4c545902f71858f3fa
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 17:59:44 +0100

    Update countDown.js

--- src/js/modules/countDown.js
+++ src/js/modules/countDown.js
@@ -4,22 +4,23 @@ const minutesElement = document.getElementById('minutes')
 const secondsElement = document.getElementById('seconds')
 const eventDate = document.getElementById('eventDate')
 
-const expirationDate = new Date(new Date().setDate(new Date().getDate() + 65)).getTime();
-eventDate.innerHTML = new Intl.DateTimeFormat('en-GB', {  day: 'numeric',month: 'short', year: 'numeric' }).format(expirationDate);
+const expirationDate = new Date(new Date().setDate(new Date().getDate() + 65)).getTime()
+eventDate.innerHTML = new Intl.DateTimeFormat('en-GB', { day: 'numeric', month: 'short', year: 'numeric' }).format(
+	expirationDate
+)
 
 setInterval(() => {
+	const now = new Date().getTime()
 
-  const now = new Date().getTime();
+	const distance = expirationDate - now
 
-  const distance = expirationDate - now;
+	const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+	const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  daysElement.innerHTML = days
-  hoursElement.innerHTML = hours
-  minutesElement.innerHTML = minutes
-  secondsElement.innerHTML = seconds
+	daysElement.innerHTML = days
+	hoursElement.innerHTML = hours
+	minutesElement.innerHTML = minutes
+	secondsElement.innerHTML = seconds
 }, 1000)
